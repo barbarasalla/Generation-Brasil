0e5bfabba42143304957674e08bf2e6c37cd87a7
Barbara Salla <barbara.msalla@gmail.com>
2022-04-04 14:56:17 -0300

    Q1: Informar todos os números de 1000 a 1999 que quando divididos por 11 obtemos resto = 5. (Usar For)

---
+++ Java/Atividade 2/Atividade2_Q1.java
@@ -0,0 +1,15 @@
+package Familia51;
+
+public class Atividade2_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for(i=1000; i<=1999; i++) {
+			if(i%11==5) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
