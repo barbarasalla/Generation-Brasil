c3dede50d3e784eb54fe2ea11a7e7110c3c8875e
Barbara Salla <barbara.msalla@gmail.com>
2022-04-05 15:54:14 -0300

    Atividade Arrays - Questao 1

---
+++ Java/Atividade 3/Atividade3_Q1.java
@@ -0,0 +1,22 @@
+package Familia51;
+
+public class Atividade3_Q1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A[]= new int[6];
+		
+		A[0]=1; A[1]=0; A[2]=5; A[3]=(-2); A[4]=(-5); A[5]=7; 
+		
+		int soma=A[0]+A[1]+A[5];
+		
+		System.out.println("A soma dos valores da posição A0 A1 e A5 foi "+soma);
+		
+		for (int i=0; i<6; i++) {
+			A[4]=100;
+			System.out.println(A[i]);
+		}		
+		
+	}
+
+}
