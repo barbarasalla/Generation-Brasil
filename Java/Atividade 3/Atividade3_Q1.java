6bd2734565680cba7b2aa495292c4c9b22279381
Barbara Salla <barbara.msalla@gmail.com>
2022-04-05 16:24:08 -0300

    modifiquei declaracao de array

--- Java/Atividade 3/Atividade3_Q1.java
+++ Java/Atividade 3/Atividade3_Q1.java
@@ -4,9 +4,7 @@ public class Atividade3_Q1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int A[]= new int[6];
-		
-		A[0]=1; A[1]=0; A[2]=5; A[3]=(-2); A[4]=(-5); A[5]=7; 
+		int A[]= {1,0,5,-2,-5,7};
 		
 		int soma=A[0]+A[1]+A[5];
 		
