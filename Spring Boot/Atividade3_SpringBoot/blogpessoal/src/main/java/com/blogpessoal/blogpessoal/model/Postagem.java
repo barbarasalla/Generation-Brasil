fe24587e45f9d6597792225ebdb3ad6ae47125ea
Barbara Salla <barbara.msalla@gmail.com>
2022-04-29 17:49:08 -0300

    Terceira Atividade de Spring, atualização do Blog Pessoal

---
+++ Spring Boot/Atividade3_SpringBoot/blogpessoal/src/main/java/com/blogpessoal/blogpessoal/model/Postagem.java
@@ -0,0 +1,90 @@
+package com.blogpessoal.blogpessoal.model;
+
+
+import java.time.LocalDateTime;
+//import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+//import javax.persistence.Temporal;
+//import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "tb_postagens")
+
+
+public class Postagem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotBlank
+	@Size(min = 3, max=100)
+	private String titulo;
+	
+	@NotBlank
+	@Size(min = 3, max=1000)
+	private String texto;
+	
+	@NotBlank
+	@UpdateTimestamp
+	//@Temporal(TemporalType.TIMESTAMP)
+	private LocalDateTime data; 
+	
+	//=new java.sql.Date(System.currentTimeMillis());
+	
+	@Size(min = 5, max=255)
+	private String foto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+	
+
+}
+
+
+
