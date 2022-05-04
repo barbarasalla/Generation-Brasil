e77ebe93bcf6fee1bc10ce4e0837366c3014de89
Barbara Salla <barbara.msalla@gmail.com>
2022-05-04 11:44:11 -0300

    Quinta atividade de Spring Boot_Farmacia

---
+++ Spring Boot/Atividade5_SpringBoot/drugstore/src/test/java/org/generation/drugstore/DrugstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.drugstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
